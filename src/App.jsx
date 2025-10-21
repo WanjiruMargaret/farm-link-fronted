098ad802f7e5517f0bd2e09da2b6e3b58f42f552
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-10-21 14:05:59 +0300

    Add tailwind CSS in project

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,12 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 export default App;
