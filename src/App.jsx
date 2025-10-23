4b86564dfa4e3b09835f10fbe32afca44ec55385
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-10-23 16:01:30 +0300

    Create AIDiagnosis, Crops, and Livestock pages and add their routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import AIDiagnosis from "./pages/AIDiagnosis";
+import Crops from "./pages/Crops";
+import Livestock from "./pages/Livestock";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/ai-diagnosis" element={<AIDiagnosis />} />
+      <Route path="/crops" element={<Crops />} />
+      <Route path="/livestock" element={<Livestock />} />
     </Routes>
   );
 }
